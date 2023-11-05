9f2e3706b871e80a5100788e4d749136e5aa7d08
ares-oykot <soykotislam131415@gmail.com>
2023-11-05 15:22:25 +0600

    added mongodb connection

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.202owzh.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+    };
+};
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
@@ -14,3 +38,8 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on port : ${port}`);
 });
+
+
+
+
+
