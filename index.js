2a742566ca575ee63ef62283768ac01db5b08bd1
ares-oykot <soykotislam131415@gmail.com>
2023-11-07 10:21:50 +0600

    Some packages are installed and setup to secure user information like jsonwebtoken, cookie-parser

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.202owzh.mongodb.net/?retryWrites=true&w=majority`;
@@ -43,6 +49,8 @@ async function run() {
             const result = await bidsCollection.insertOne(bidInfo);
             res.send(result);
         });
+
+        
 
 
         await client.db("admin").command({ ping: 1 });
