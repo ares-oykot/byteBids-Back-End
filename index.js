5e14610c3ac230adce226477d932a11f8738aacc
ares-oykot <soykotislam131415@gmail.com>
2023-11-07 18:01:15 +0600

    added deleted related api

--- index.js
+++ index.js
@@ -24,11 +24,31 @@ const client = new MongoClient(uri, {
     }
 });
 
+const verifyToken = async (req, res, next) => {
+    const token = req?.cookies?.token;
+    if (!token) {
+        return res.status(401).send({ message: 'not authorized' })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized' });
+        }
+        req.user = decoded;
+        next();
+    });
+};
+
 async function run() {
     try {
         await client.connect();
         const jobsCollection = client.db('ByteBidsDB').collection('Jobs');
         const bidsCollection = client.db('ByteBidsDB').collection('bids');
+
+        app.post('/jobs', async (req, res) => {
+            const jobInfo = req.body;
+            const result = await jobsCollection.insertOne(jobInfo);
+            res.send(result);
+        });
 
         app.get('/jobs/:category', async (req, res) => {
             const category = req.params.category;
@@ -44,15 +64,31 @@ async function run() {
             res.send(result);
         });
 
+        app.get('/jobs', verifyToken, async (req, res) => {
+            if (req.query.email !== req.user.email) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+            let query = {};
+            if (req.query?.email) {
+                query = { buyer_Email: req.query?.email }
+            }
+            const cursor = jobsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
         app.post('/bids', async (req, res) => {
             const bidInfo = req.body;
             const result = await bidsCollection.insertOne(bidInfo);
             res.send(result);
         });
 
-        app.get('/bids', async (req, res) => {
+        app.get('/bids', verifyToken, async (req, res) => {
+            if (req.query.email !== req.user.email) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
             let query = {};
-            if(req.query?.email){
+            if (req.query?.email) {
                 query = { email: req.query?.email }
             }
             const cursor = bidsCollection.find(query);
@@ -62,13 +98,20 @@ async function run() {
 
         app.post('/jwt', async (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, process.env.Secret, { expiresIn: '1h' });
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
             res
                 .cookie('token', token, {
                     httpOnly: true,
                     secure: false,
                 })
                 .send({ success: true });
+        });
+
+        app.delete('/jobs/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await jobsCollection.deleteOne(query);
+            res.send(result);
         });
 
         await client.db("admin").command({ ping: 1 });
